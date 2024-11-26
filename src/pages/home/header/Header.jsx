93bb1770d76af3d028cd1a3c3af27d3a2228388b
AnderlynDePaz <anderlyndepazhurtado@gmail.com>
2024-11-26 10:11:52 +0100

    Add/finish

---
+++ src/pages/home/header/Header.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import * as FaIcons from 'react-icons/fa';
+import css from "./header.module.scss";
+import logo from '../../../assets/pokemon.png';
+
+export default function Header({ obtenerSearch }) {
+    return (
+        <nav className={css.header}>
+            <div className={css.div_header}>
+                <div className={css.div_logo}>
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className={css.div_search}>
+                    <div>
+                        <FaIcons.FaSearch />
+                    </div>
+                        <input
+                            type="search"
+                            onChange={(e) => obtenerSearch(e.target.value)}
+                            placeholder="Buscar Pokémon..." className="buscar"
+                        />
+                        <button type="submit">Buscar</button>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
